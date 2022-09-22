f5ea6cfbc8e0b9666a66b3e40c82d85d4bc5e334
lupete3 <lupeteplacide@gmail.com>
2022-09-22 10:28:04 +0100

    Add proper pages titles

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,5 +1,7 @@
 @extends('base')
 
+@section('title',' About Us | Hello City')
+   
 @section('content')
     
     <h1>Build with &hearts; Pl@cid</h1>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,7 @@
 @extends('base')
 
+@section('title','Hello City')
+    
 @section('content')
         
     <h1>Hello From DRC</h1>
@@ -7,4 +9,4 @@
         
     <p>It's currently {{ date('h:i A') }} </p>
        
-@endsection
+@stop
