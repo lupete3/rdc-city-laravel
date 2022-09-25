9dd5178b2deb9734b5aa5ca4c5409b6ba97db0a8
lupete3 <lupeteplacide@gmail.com>
2022-09-25 08:21:11 +0100

    Make user Route::view of intead Route::get

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function(){
-    return view('pages/home');
-})->name('home');
 
-Route::get('/about-us', function(){
-    return view('pages.about');
-})->name('about');
+Route::view('/', 'pages/home')->name('home');
+Route::view('/about-us', 'pages/about')->name('about');
